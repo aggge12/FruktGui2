b4a9a9d64101382fb5d56d557019143360a57702
Axel <axel.lundberg.gbg@hotmail.com>
2017-09-18 15:48:00 +0200

    first

---
+++ FruktAdminApp/Fruit.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FruktAdminApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Fruit : Page
+    {
+        public Fruit()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Back_click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
+        private void FindItem(object sender, RoutedEventArgs e)
+        {
+            //string tmp = inputName.Text;
+
+             // something something api request
+
+            // new page or something?
+        }
+        
+    }
+}
